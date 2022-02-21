5444691338e4d4d95ed229eb7a2c357ddc5869bd
Hieu Vu Duc Nguyen <vdhieu01@gmail.com>
2022-02-20 22:02:04 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let speech
+  if (distance < 400)
+    return speech = 'This one is on me!';
+  else if (distance > 2000 && distance < 2500 )
+    return speech = 'I will gladly take your thirty bucks.';
+  else if (distance >2000 && distance > 2500)
+    return speech = "No can do.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(destination){
+  let speech
+  if (destination ==='NYC')
+    return speech ='Ok, sounds good.';
+  else return speech = "No go.";}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let speech 
+  if (tip === "generous" )
+    return speech = "Thank you so much.";
+  else if (tip ==='not as generous')
+    return speech = "Thank you.";
+  else if (tip !== "generous" && tip !== "not as generous")
+    return speech = "Bye.";
 }
